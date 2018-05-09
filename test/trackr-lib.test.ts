a684979414e4dc20710829fda6d2bfb1d36691f5
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-05-09 14:24:57 +0200

    refactor: remove useless import in tests

--- test/trackr-lib.test.ts
+++ test/trackr-lib.test.ts
@@ -1,10 +1,8 @@
-import * as trackr from '../src/app.ts'
-
 /**
  * Dummy test
  */
 describe('Dummy test', () => {
   it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
-})
+    expect(true).toBeTruthy();
+  });
+});
